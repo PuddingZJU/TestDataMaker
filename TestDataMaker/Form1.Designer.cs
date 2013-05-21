75b0ee55a10b621d4c05ccfaa46261560b59121c
codeknight <leonroxas@gmail.com>
2013-05-21 16:30:33 +0800

    first commit

---
+++ TestDataMaker/Form1.Designer.cs
@@ -0,0 +1,416 @@
+﻿namespace TestDataMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.RunButton = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.resetbutton = new System.Windows.Forms.Button();
+            this.dimensionbox = new System.Windows.Forms.ComboBox();
+            this.weakbox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.databox = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.ErrorBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(140, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(140, 127);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "2";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(140, 153);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "3";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(140, 179);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "4";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(140, 205);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // RunButton
+            // 
+            this.RunButton.Location = new System.Drawing.Point(165, 400);
+            this.RunButton.Name = "RunButton";
+            this.RunButton.Size = new System.Drawing.Size(58, 22);
+            this.RunButton.TabIndex = 5;
+            this.RunButton.Text = "Run";
+            this.RunButton.UseVisualStyleBackColor = true;
+            this.RunButton.Click += new System.EventHandler(this.RunButton_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(140, 231);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 10;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(140, 257);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 9;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(140, 283);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 8;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(140, 309);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 7;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(140, 335);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 6;
+            // 
+            // resetbutton
+            // 
+            this.resetbutton.Location = new System.Drawing.Point(58, 400);
+            this.resetbutton.Name = "resetbutton";
+            this.resetbutton.Size = new System.Drawing.Size(58, 22);
+            this.resetbutton.TabIndex = 11;
+            this.resetbutton.Text = "Reset";
+            this.resetbutton.UseVisualStyleBackColor = true;
+            this.resetbutton.Click += new System.EventHandler(this.resetbutton_Click);
+            // 
+            // dimensionbox
+            // 
+            this.dimensionbox.FormattingEnabled = true;
+            this.dimensionbox.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.dimensionbox.Location = new System.Drawing.Point(140, 44);
+            this.dimensionbox.Name = "dimensionbox";
+            this.dimensionbox.Size = new System.Drawing.Size(62, 21);
+            this.dimensionbox.TabIndex = 12;
+            this.dimensionbox.SelectedIndexChanged += new System.EventHandler(this.dimensionbox_SelectedIndexChanged);
+            // 
+            // weakbox
+            // 
+            this.weakbox.FormattingEnabled = true;
+            this.weakbox.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.weakbox.Location = new System.Drawing.Point(140, 71);
+            this.weakbox.Name = "weakbox";
+            this.weakbox.Size = new System.Drawing.Size(62, 21);
+            this.weakbox.TabIndex = 13;
+            this.weakbox.SelectedIndexChanged += new System.EventHandler(this.weakbox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "弱分类器数：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "特征数：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "1号特征阈值：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "2号特征阈值：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "4号特征阈值：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(49, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "3号特征阈值：";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(49, 234);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "6号特征阈值：";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(49, 208);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "5号特征阈值：";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(49, 286);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(85, 13);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "8号特征阈值：";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(49, 260);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(85, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "7号特征阈值：";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(43, 338);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(91, 13);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "10号特征阈值：";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(49, 312);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(85, 13);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "9号特征阈值：";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(12, 372);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(67, 13);
+            this.label13.TabIndex = 27;
+            this.label13.Text = "数据个数：";
+            // 
+            // databox
+            // 
+            this.databox.Location = new System.Drawing.Point(85, 369);
+            this.databox.Name = "databox";
+            this.databox.Size = new System.Drawing.Size(52, 20);
+            this.databox.TabIndex = 26;
+            this.databox.Text = "100";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(144, 372);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(114, 13);
+            this.label14.TabIndex = 28;
+            this.label14.Text = "正确率：                 %";
+            // 
+            // ErrorBox
+            // 
+            this.ErrorBox.Location = new System.Drawing.Point(196, 369);
+            this.ErrorBox.Name = "ErrorBox";
+            this.ErrorBox.Size = new System.Drawing.Size(44, 20);
+            this.ErrorBox.TabIndex = 29;
+            this.ErrorBox.Text = "100";
+            this.ErrorBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 450);
+            this.Controls.Add(this.ErrorBox);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.databox);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.weakbox);
+            this.Controls.Add(this.dimensionbox);
+            this.Controls.Add(this.resetbutton);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.RunButton);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "TestDataMaker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button RunButton;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Button resetbutton;
+        private System.Windows.Forms.ComboBox dimensionbox;
+        private System.Windows.Forms.ComboBox weakbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox databox;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox ErrorBox;
+    }
+}
+
---
+++ TestDataMaker/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+namespace TestDataMaker
+{
+    public partial class Form1 : Form
+    {
+        TextBox[] tblist = new TextBox[10];
+        Random ran;
+        public Form1()
+        {
+            InitializeComponent();
+
+            tblist[0] = textBox1;
+            tblist[1] = textBox2;
+            tblist[2] = textBox3;
+            tblist[3] = textBox4;
+            tblist[4] = textBox5;
+            tblist[5] = textBox6;
+            tblist[6] = textBox7;
+            tblist[7] = textBox8;
+            tblist[8] = textBox9;
+            tblist[9] = textBox10;
+            ran = new Random((int)(DateTime.Now.Ticks & 0xffffffffL) | (int)(DateTime.Now.Ticks >> 32));
+            dimensionbox.SelectedIndex = 9;
+            weakbox.SelectedIndex = 9;
+
+            for (int i = 0; i < tblist.Length; i++)
+            {
+                tblist[i].Text = ran.Next(10000).ToString();
+                tblist[i].Enabled = true;
+            }
+        }
+
+        private void RunButton_Click(object sender, EventArgs e)
+        {
+            int[] thr ;
+            int datacnt;
+            int weaknum;
+            double error;
+            try
+            {
+                thr = new int[Convert.ToInt32(dimensionbox.Text)];
+                datacnt = Convert.ToInt32(databox.Text);
+                weaknum = Convert.ToInt32(weakbox.SelectedItem);
+                error = Convert.ToDouble(ErrorBox.Text);
+                error /= 100.0;
+            }
+            catch (Exception ice)
+            {
+                MessageBox.Show(ice.ToString());
+                return;
+            }
+            for (int i = 0; i < thr.Length; i++)
+            {
+                try
+                {
+                    thr[i] = Convert.ToInt32(tblist[i].Text);
+                    if (thr[i] <= 0)
+                    {
+                        MessageBox.Show((i + 1).ToString() + "号阈值有误！请输入小于10000的正整数");
+                        return;
+                    }
+                    if (thr[i] > 10000)
+                    {
+                        MessageBox.Show((i + 1).ToString() + "号阈值过大！请输入小于10000的正整数");
+                        return;
+                    }
+                }
+                catch (Exception ice)
+                {
+                    MessageBox.Show((i + 1) + "号阈值填写错误！\n" + ice.ToString());
+                    return;
+                }
+            }
+            StreamWriter sw = new StreamWriter(@"D:\alldata.txt");
+            string testdat="";
+            int cntID=0;
+            for (int i = 0; i < datacnt*error; i++,cntID++)
+            {
+
+                string line = (cntID + 1).ToString()+"\t1";
+                testdat += (cntID + 201).ToString()+"\t1";
+                for (int j = 0; j < thr.Length; j++)
+                {
+                    string tmp = "\t"+ran.Next(thr[j], thr[j] * 2).ToString();
+                    line+=tmp;
+                    testdat += tmp;
+                }
+                sw.WriteLine(line);
+                testdat += "\r\n";
+            }
+            for (int i = 0; i < datacnt * (1-error); i++,cntID++)
+            {
+
+                string line = (cntID + 1).ToString() + "\t10";
+                testdat += (cntID + 201).ToString() + "\t10";
+                for (int j = 0; j < thr.Length; j++)
+                {
+                    string tmp = "\t" + ran.Next(thr[j], thr[j] * 2).ToString();
+                    line += tmp;
+                    testdat += tmp;
+                }
+                sw.WriteLine(line);
+                testdat += "\r\n";
+
+            }
+            for (int i = 0; i < datacnt * (1 - error); i++,cntID++)
+            {
+
+                string line = (cntID + 1).ToString() + "\t1";
+                testdat += (cntID + 201).ToString() + "\t1";
+                for (int j = 0; j < thr.Length; j++)
+                {
+                    string tmp = "\t" + ran.Next(0, thr[j]).ToString();
+                    line += tmp;
+                    testdat += tmp;
+                }
+                sw.WriteLine(line);
+                testdat += "\r\n";
+
+            }
+            for (int i = 0; i < datacnt*error; i++,cntID++)
+            {
+                string line = (cntID + 1).ToString() + "\t10";
+                testdat += (cntID + 201).ToString() + "\t10";
+                for (int j = 0; j < thr.Length; j++)
+                {
+                   string tmp = "\t" + ran.Next(0, thr[j]).ToString();
+                    line += tmp;
+                    testdat += tmp;
+                }
+                sw.WriteLine(line);
+                testdat += "\r\n";
+
+            }
+            sw.Write(testdat);
+            sw.Close();
+            sw = new StreamWriter(@"D:\config.txt");
+            sw.WriteLine("200\t399\t"+weakbox.Text);
+            sw.Close();
+            //MessageBox.Show("ok");
+            Process app = new System.Diagnostics.Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo(); 
+            startInfo.FileName= @"D:\Documents\Visual Studio 2012\Projects\Adaboost_V\Release\Adaboost_V.exe";
+            startInfo.Arguments = @"D:\alldata.txt D:\config.txt";
+            app.StartInfo = startInfo;
+            app.Start();
+        }
+
+        private void dimensionbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int dim = Convert.ToInt32(dimensionbox.SelectedItem);
+            for (int i = 0; i < 10; i++)
+            {
+                tblist[i].Enabled = i < dim;
+                tblist[i].Text = i < dim ? tblist[i].Text : "";
+            }
+        }
+
+        private void weakbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resetbutton_Click(object sender, EventArgs e)
+        {
+
+            dimensionbox.SelectedIndex = 9;
+            weakbox.SelectedIndex = 9;
+            
+            for (int i = 0; i < tblist.Length; i++)
+            {
+                tblist[i].Text = ran.Next(10000).ToString();
+                tblist[i].Enabled = true;
+            }
+        }
+    }
+}
